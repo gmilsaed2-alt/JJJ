2595b4116cae1518f98421123ec7dc2ee56f28c3
gmilsaed2-alt <gmilsaed2@gmail.com>
2026-01-07 02:13:28 +0300

    Add Firebase configuration file

---
+++ src/app/src/app/src/lib/src/lib/src/lib/docs/src/app/src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1478262916-fc728",
+  "appId": "1:614065956580:web:5448536054edb8d0246ef1",
+  "apiKey": "AIzaSyBAi1_ewPZGztkxZbaYLbPmFlgTDptH4sg",
+  "authDomain": "studio-1478262916-fc728.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "614065956580"
+};
